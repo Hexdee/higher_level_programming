acf3a088683f5b8c84076248076ac5c2b0d6f5ba
Hexdee <lukmansaheed79@gmail.com>
2022-06-02 12:55:08 +0100

    Did task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    argc = len(argv) - 1
+    print("{:d} argument{}{}".format(argc, "s" if argc != 1 else "",
+                                 "." if argc == 0 else ":"))
+    if argc > 0:
+        for i in range(1, argc + 1):
+            print("{:d}: {}".format(i, argv[i]))
