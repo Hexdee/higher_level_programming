05c0bd05d17ee8721bfd6351b29d5bb31c76d7b6
hexdee <lukmansaheed79@gmail.com>
2022-06-24 12:07:12 +0000

    Add documnentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,13 @@
-#!/bin/python3
+#!/usr/bin/python3
+"""A module containing square"""
 
 class Square:
+    """Sqaure class with initialized with size"""
+
     def __init__(self, size):
+        """Initiapize sqaure with size
+        Args:
+            size (int): size of tge square
+
+        """
         self.__size = size
