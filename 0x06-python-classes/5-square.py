331e538feacbe82c3da1739607b754aefd9f3e46
hexdee <lukmansaheed79@gmail.com>
2022-06-24 13:54:10 +0000

    Add doc for task 5 and 6

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,14 +1,36 @@
 #!/usr/bin/python3
+"""A module containing a square"""
+
+
 class Square:
+    """A square class"""
+
     def __init__(self, size=0):
+        """initializes square
+        Args:
+            size (int): size of the square
+        """
+
         self.size = size
 
     @property
     def size(self):
+        """Gets value of size
+
+        Returns:
+            size (int)
+        """
+
         return self.__size
 
     @size.setter
     def size(self, value):
+        """ Change the value of size
+
+        Args:
+            value (int): new value of size
+        """
+
         if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -16,10 +38,17 @@ class Square:
         self.__size = value
 
     def area(self):
-        a = self.__size * self.__size
-        return a
+        """ Calculates the area of a square
+
+        Returns:
+            area
+        """
+
+        return self.__size * self.__size
 
     def my_print(self):
+        """Print a square"""
+
         if self.__size == 0:
             print("")
         for i in range(0, self.__size):
--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,27 +1,64 @@
 #!/usr/bin/python3
+"""A module containing a square"""
+
+
 class Square:
-    def __init__(self, size=0, position=(0, 0)):
+    """A square class"""
+
+    def __init__(self, size=0):
+        """initializes square
+        Args:
+            size (int): size of the square
+        """
+
         self.size = size
-        self.position = position
 
     @property
     def size(self):
+        """Gets value of size
+
+        Returns:
+            size (int)
+        """
+
         return self.__size
 
     @size.setter
     def size(self, value):
+        """ Change the value of size
+
+        Args:
+            value (int): new value of size
+        """
+
         if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
+    def area(self):
+        """ Calculates the area of a square
+
+        Returns:
+            area
+        """
+
+        return self.__size * self.__size
+
     @property
     def position(self):
+        """gets the position"""
+
         return self.__position
 
     @position.setter
     def position(self, value):
+        """change the value of poaition
+        Args:
+            value (int, int): the new position
+        """
+
         if type(value) != tuple or len(value) != 2:
             raise TypeError("position must be a tuple of 2 positive integers")
         if type(value[0]) != int or type(value[1]) != int:
@@ -35,6 +72,8 @@ class Square:
         return a
 
     def my_print(self):
+        """Print a square"""
+
         if self.__size == 0:
             print("")
         else:
