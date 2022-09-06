11ff57945075f1e8b4afc356f903052282bd86bb
Hexdee <lukmansaheed79@gmail.com>
2022-09-06 15:25:38 +0100

    Did javascript warmup 8-17

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,5 +4,5 @@ const myNum = process.argv[2];
 if (isNaN(myNum)) {
   console.log('Not a number');
 } else {
-  console.log('My number: ', myNum);
+  console.log('My number:', myNum);
 }
--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,9 +3,9 @@
 const x = process.argv[2];
 
 if (isNaN(x)) {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 }
 
 for (let i = 0; i < x; i++) {
-	console.log('C is fun');
+  console.log('C is fun');
 }
