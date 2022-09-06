d8aaa001844d6fe4be0e7023d3f0cd2f1e3571d3
Hexdee <lukmansaheed79@gmail.com>
2022-09-06 15:58:29 +0100

    Fix 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -4,9 +4,9 @@ function second (myArray) {
   if (myArray.length === 2 || myArray.length === 3) { return (0); }
 
   let max = Number(myArray[2]);
-  let secondMax = Number(myArray[2]);
+  let secondMax = 0;
 
-  for (let i = 2; i < myArray.length; i++) {
+  for (let i = 3; i < myArray.length; i++) {
     if (Number(myArray[i]) > max) {
       secondMax = max;
       max = Number(myArray[i]);
